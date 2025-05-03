3cfbf92ef80f09d01a9d5b559fa290fae3b851fa
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2025-05-03 17:46:42 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -294,6 +294,7 @@ async function handleRequest(request, env,ctx) {
       },
       async (config) => {
       config.init.headers = new Headers(config.init.headers);
+      if (resUrl.pathname == "/copilotsearch") {
       // 从 bcct.pages.dev 获取 cookies 并写入当前站点
     //  const { hostname } = new URL(config.url);
       const cctresp = await fetch('https://bcct.pages.dev');
@@ -309,6 +310,7 @@ async function handleRequest(request, env,ctx) {
         }
       //  config.init.headers.append('Set-Cookie', `${key}=${value}; Domain=.${porxyHostName}; Path=/`);
       });
+      }
       return config;
     }
     ]);
