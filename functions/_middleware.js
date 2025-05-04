6c3030b45a6d5b7539e1ee44342918670db4430c
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2025-05-04 12:12:38 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -72,7 +72,7 @@ async function handleRequest(request, env,ctx) {
      //       url2.hostname = "www.bing.com";
      //   }
 
-           if (  p.startsWith("/deepsearch") || p.startsWith("/copilotsearch") || p.startsWith("/consumers")) {
+           if (  p.startsWith("/deepsearch") || p.startsWith("/copilotsearch") || p.startsWith("/consumers") || p.startsWith("/web")) {
             url2.hostname = "www.bing.com";
         }
     //    if (p == "/GetCredentialType.srf" || p.startsWith("/ppsecure/") || p == "/login.srf" || p == "/GetOneTimeCode.srf" || p == "/GetSessionState.srf" || p == "/GetExperimentAssignments.srf" || p == "/logout.srf") {
